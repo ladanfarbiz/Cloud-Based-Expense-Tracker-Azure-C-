d50126f0bfe160bc1cf1e6d061947df041a5db52
Ladan Farbiz <127969409+ladanfarbiz@users.noreply.github.com>
2025-02-18 17:03:41 +0100

    expenses.cs

---
+++ expenses.cs
@@ -0,0 +1,7 @@
+public class Expense
+{
+    public string Id { get; set; }
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+}
